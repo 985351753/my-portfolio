c7059e0647178c732b8ae5db98a799b0f0ee7a13
985351753 <shizubiz32@gmail.com>
2026-02-02 19:24:32 +0900

    2026-02-02-1924

--- src/scripts/spilitText.js
+++ src/scripts/spilitText.js
@@ -4,10 +4,10 @@ import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(SplitText);
 
-const split = new SplitText(".js-split", { type: "chars" });
+const split = SplitText.create(".js-split", { type: "chars" });
 
-gsap.fromTo(
-  split.chars,
-  { opacity: 1, y: "110%" },
-  { opacity: 1, y: 0, duration: 2, ease: "power3.out", stagger: 0.2 }
-);
+gsap.from(split.chars,  {
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+  });
