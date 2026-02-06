250e8c6e45e32202f63a2e02dbd89d76b11dbc11
985351753 <shizubiz32@gmail.com>
2026-02-06 14:47:13 +0900

    ファイル移動

--- public/src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { SplitText } from "./SplitText";
-
-document.addEventListener("DOMContentLoaded", () => {
-  new SplitText(".js-s-block", {
-    target: ".js-s-text",
-    transition: [400],
-  });
-});
